96ff1903d4024356392d11b8bbcc1417ecbec976
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-12-06 20:11:06 -0200

    Added proxy and tick-tack

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import { clearInterval } from 'timers';
 
 class App extends Component {
 
-  state = {users: [], error: false}
+  constructor(props) {
+    super(props);
+    
+    this.state = {users: [], error: false, counter: "Loading ..."};
+
+    this.addUser = this.addUser.bind(this);
+    this.fetchCounter = this.fetchCounter.bind(this);
+  }
   
-  componentDidMount() {
+  addUser() {
+    fetch('/api/users/add');
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
     fetch('/api/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users, error: false }))
-      .catch(() => this.setState({ users: [], error: true }));
+    .then(res => res.json())
+    .then(users => this.setState({ users, error: false }))
+    .catch(() => this.setState({ users: [], error: true }));
+  }
+
+  fetchCounter() {
+    fetch('/api/users/counter')
+    .then(res => res.text())
+    .then(counter => this.setState({ counter }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+    this.setState({ intervalId: setInterval(this.fetchCounter, 3000) });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
   }
 
   render() {
     return (
       <div className="App">
         <h1>SmartGrow</h1>
+
+        Tick-tack: {this.state.counter} 
+
+        <br/>
+        
+        <button onClick={this.addUser}>Add User</button>
         {this.state.error &&
           <div>Some error occurred while trying to fetch users.</div>
         }
